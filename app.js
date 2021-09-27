5ece61f1775e718dbae5ef245036354cc91d155a
abdullah-manzoor <abdullah.manzoor@intellicon.io>
2021-09-27 14:06:39 +0500

    OE

---
+++ app.js
@@ -0,0 +1,40 @@
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
+
+let indexRouter = require('./routes/index');
+let usersRouter = require('./routes/users');
+let oeRouter = require('./routes/oe');
+
+let app = express();
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/orders', oeRouter);
+
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ routes/oe.js
@@ -0,0 +1,72 @@
+let express = require('express');
+let router = express.Router();
+let axios = require('axios');
+let FormData = require('form-data');
+
+let TOKEN = null;
+let data = new FormData();
+data.append('username', 'intellicon.insignia@gmail.com');
+data.append('password', 'Admin@123');
+data.append('grant_type', 'password');
+let config = {
+  method: 'post',
+  url: 'https://oeprocessingstage.shopistan.pk/oauth/token',
+  headers: {
+    'Authorization': 'Basic c2hvcGlzdGFuQ2xpZW50QXBwOmxpYmlhbW9DYWxpY2k=',
+    ...data.getHeaders()
+  },
+  data: data
+};
+axios(config)
+  .then(response => {
+    TOKEN = response.data.access_token;
+  })
+  .catch(e => {
+    console.log(e)
+  })
+
+
+/* GET users listing. */
+router.get('/:id/autoverify', function (req, res, next) {
+  let orderId = req.params.id;
+  console.log(orderId, TOKEN);
+  let config = {
+    method: 'get',
+    url: `https://oeprocessingstage.shopistan.pk/orders/${ orderId }/autoverify/`,
+    headers: {
+      'Authorization': `Bearer ${ TOKEN }`,
+      'client_type': 'ivr'
+    }
+  };
+  return axios(config)
+    .then(resp => {
+      console.log(resp.data)
+      res.json(resp.data);
+    })
+    .catch(e => {
+      res.status(500)
+    })
+});
+
+router.get('/:id/dead', function (req, res, next) {
+  let orderId = req.params.id;
+  console.log(orderId, TOKEN);
+  let config = {
+    method: 'get',
+    url: `https://oeprocessingstage.shopistan.pk/orders/${ orderId }/dead/`,
+    headers: {
+      'Authorization': `Bearer ${ TOKEN }`,
+      'client_type': 'ivr'
+    }
+  };
+  return axios(config)
+    .then(resp => {
+      console.log(resp.data)
+      res.json(resp.data);
+    })
+    .catch(e => {
+      res.status(500)
+    })
+});
+
+module.exports = router;
